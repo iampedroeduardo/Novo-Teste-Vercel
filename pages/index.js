a1fe10de7bce631a9c85986308658ca37572a80c
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-09-03 22:06:42 -0300

    conserto

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function index(){
+    return (<>Nome:<input id="nome" type="text"></input>
+    Autor:<input id="autor" type="text"></input>
+    Descrição:<input id="descricao" type="text"></input>
+    Isbn:<input id="isbn" type="text"></input>
+    Editora:<input id="editora" type="text"></input>
+    <button>Cadastrar</button>
+    <button>Mostrar</button></>);
+}
